ed74613ecff8cbe9dc4f45aa4260969c3daa8bb5
Angélica Alves <angelicavitoriia@hotmail.com>
2022-07-23 09:37:12 -0300

    first commit

---
+++ src/com/br/poo/Aula.java
@@ -0,0 +1,21 @@
+package com.br.poo;
+
+public class Aula {
+
+	public static void main(String[] args) {
+		 
+	 
+		Caneta c1 = new  Caneta();
+		c1.cor = "azul";
+		c1.ponta = 0.5f;
+		c1.destampar();
+		c1.status();
+		c1.rabiscar();
+	
+		
+	
+}
+	
+	
+
+}
